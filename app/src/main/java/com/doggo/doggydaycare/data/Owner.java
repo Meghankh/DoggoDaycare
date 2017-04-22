dc02a0c509ee188a7427e028e5f2bd6fc1d15c0c
Meghan Hamannwright <meghankh@vt.edu>
2017-04-22 18:08:53 -0400

    added owner model

---
+++ app/src/main/java/com/doggo/doggydaycare/data/Owner.java
@@ -0,0 +1,44 @@
+package com.doggo.doggydaycare.data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by meghankh on 4/22/2017.
+ */
+
+public class Owner {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Dog> dogs;
+
+    public Owner(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        dogs = new ArrayList<Dog>();
+    }
+
+    public void addDog(Dog dog)
+    {
+        dogs.add(dog);
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Dog> getDogs() {
+        return dogs;
+    }
+
+}
